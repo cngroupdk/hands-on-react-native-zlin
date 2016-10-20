66cee618944ea6b8b1070122cc7782ace1cb0cda
Tomáš Horáček <tomas.horacek@gmail.com>
2016-10-20 20:15:29 +0200

    Hastags

--- HandsOnReactNative-Workshop/index.android.js
+++ HandsOnReactNative-Workshop/index.android.js
@@ -13,48 +13,38 @@ import {
 } from 'react-native';
 
 import DATA from './simple.json';
-
 export default class AwesomeProject extends Component {
   render() {
     const groups = Object.keys(DATA.groups);
     return (
       <View style={styles.container}>
         {groups.map((groupTitle, index) =>
-          <View key={index}>
-            <Text>{groupTitle}</Text>
+          <View style={styles.hastagWrapper}  key={index}>
+            <Text style={styles.hastagTitle}>#{groupTitle}</Text>
           </View>
         )}
-        <Text style={styles.welcome}>
-          {groups.join(', ')}
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  hastagTitle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  hastagWrapper: {
+    backgroundColor: 'gray',
+    alignSelf: 'stretch',
+    padding: 5,
+  },
   container: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
